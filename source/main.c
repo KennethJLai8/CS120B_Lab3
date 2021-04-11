1b3879f236bfc9712f14e1175120ad7e0eab225d
Kenneth Lai <klai022@wch136-22.cs.ucr.edu>
2021-04-11 14:32:17 -0700

    lab 3 part 2 initial commit

--- source/main.c
+++ source/main.c
@@ -1,7 +1,9 @@
+
+
 /*	Author: klai022
- *  Partner(s) Name: 
+ *	Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab 3  Exercise 1
+ *	Assignment: Lab 3  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,35 +15,40 @@
 #endif
 int main(void) {
 DDRA = 0x00; PORTA = 0xFF; 
-DDRB = 0x00; PORTB = 0XFF;
 DDRC = 0xFF; PORTC = 0x00; //C is an output
 
 while(1) {
 
-unsigned char input_A = PINA;
-unsigned char input_B = PINB;
+unsigned char input_A = PINA & 0xFF;//fuel level
+unsigned char output_C = 0x00;
 
-unsigned char i;
-unsigned char j;
-unsigned char cnt = 0x00;
 
-for(i = 0; i < 8; i++){
-	input_A = input_A >> i;
-	
-	if(input_A & 0x01){
-	cnt++;
-	}
+if((input_A & 0x01) || (input_A & 0x02)){
+    output_C = output_C | 0x20;
+}
+else if((input_A & 0x03) || (input_A & 0x04)){
+    output_C = output_C | 0x30;
+}
+else if((input_A & 0x05) || (input_A & 0x06)){
+    output_C = output_C | 0x38;
+}
+else if((input_A & 0x07) || (input_A & 0x08) || (input_A & 0x09)){
+    output_C = output_C | 0x3C;
+}
+else if((input_A & 0x0A) || (input_A & 0x0B) || (input_A & 0x0C)){
+    output_C = output_C | 0x3D;
 }
 
-for(j = 0; j < 8; j++){
-        input_B = input_B >> i;
-
-        if(input_B & 0x01){
-        cnt++;
-        }
+if((input_A & 0x01) || (input_A & 0x02) || (input_A & 0x03) || (input_A & 0x04)){
+    output_C = output_C | 0x40;
 }
 
-PORTC = cnt;
+
+
+
+
+PORTC = output_C;
 }
 return 0;
 }
+
